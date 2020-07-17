c76b04923a3310f19a39dad6282e46ac14c021cf
Sonechok <olexandrhl000@gmail.com>
2020-07-17 23:18:01 +0300

    Homework jdbc

---
+++ Java/src/main/java/lesson2_jdbc/MainLesson2.java
@@ -0,0 +1,35 @@
+package lesson2_jdbc;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainLesson2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Connection connection = ConnectionManager.createConnection();
+        BookRepository bookRepository = new BookRepository(connection);
+        AuthorRepository authorRepository = new AuthorRepository(connection);
+        String email;
+        int min;
+        int max;
+        System.out.println("Enter email address of author you want to find");
+        email = scanner.nextLine();
+        Author author = authorRepository.findByEmail(email);
+        if (author == null){
+            System.out.println("There is no such author with this email");
+        } else {
+            System.out.println(author);
+        }
+        System.out.println("Enter min price of books you want to get");
+        min = scanner.nextInt();
+        System.out.println("Enter max price of books you want to get");
+        max = scanner.nextInt();
+        List<Book> books = bookRepository.findByRangeOfPrice(min, max);
+        if (books.isEmpty()){
+            System.out.println("List is empty");
+        } else {
+            System.out.println(books);
+        }
+    }
+}
