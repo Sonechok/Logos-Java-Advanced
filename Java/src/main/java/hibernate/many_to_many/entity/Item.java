ebe73e51993bd2572b7f1a519b19fd08b184fb76
Sonechok <olexandrhl000@gmail.com>
2020-07-17 23:52:33 +0300

    Homework hibernate mapping many to many

---
+++ Java/src/main/java/hibernate/many_to_many/entity/Item.java
@@ -0,0 +1,43 @@
+package hibernate.many_to_many.entity;
+
+import java.util.Set;
+
+public class Item {
+    private Integer id;
+
+    private Integer total;
+
+    private Set<Cart> cartSet;
+
+    public Item(Integer total) {
+        this.total = total;
+    }
+
+    public Item() {
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Set<Cart> getCartSet() {
+        return cartSet;
+    }
+
+    public void setCartSet(Set<Cart> cartSet) {
+        this.cartSet = cartSet;
+    }
+}
